db04d580aba29e5cc38913b8a2cb53d9877db859
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-11 20:32:16 +0530

    updated manifest perms

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,7 +1,7 @@
 chrome.devtools.panels.create("My panel", "images/icon-16.png", "panel.html", function(panel) {
 	chrome.devtools.network.onRequestFinished.addListener(
 		function(request) {
-			var body = document.getElementById("test");
+			var body = document.getElementById("body");
 			body.innerHTML = request.request.getHAR();
 			console.log(request.request.getHAR())
 		}

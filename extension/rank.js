097aa82f0ca81115801de9345c0db50c5bd32d16
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-12 06:14:52 +0530

    updated rank page

--- extension/rank.js
+++ extension/rank.js
@@ -1,10 +1,10 @@
 var myArray = [
-    { 'rank': '1', 'website': 'google.com', 'carbon': '0.111989' },
+    { 'rank': '1', 'website': 'google.com', 'carbon': '1.211989' },
     { 'rank': '2', 'website': 'gmail.com', 'carbon': '1.11989' },
-    { 'rank': '3', 'website': 'yahoo.com', 'carbon': '0.141990' },
-    { 'rank': '4', 'website': 'outlook.com', 'carbon': '1.291993' },
+    { 'rank': '3', 'website': 'yahoo.com', 'carbon': '0.441990' },
+    { 'rank': '4', 'website': 'youtube.com', 'carbon': '0.391993' },
     { 'rank': '5', 'website': 'instagram.com', 'carbon': '0.3121991' },
-    { 'rank': '6', 'website': 'youtube.com', 'carbon': '1.0311995' },
+    { 'rank': '6', 'website': 'outlook.com', 'carbon': '0.0311995' },
 ]
 
 buildTable(myArray)
